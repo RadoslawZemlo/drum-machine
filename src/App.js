edc58581d42b29ee4d68cfe0b6fd1f3e7a50962b
RadoslawZemlo <radoslawzemlo1@gmail.com>
2022-04-22 12:39:36 +0200

    Setup react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Drum Maschine</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
